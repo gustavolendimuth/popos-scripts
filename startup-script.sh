5882c19254dbbc52692bbb8dc2ba4ec94b369efb
Gustavo Lendimuth <gustavolendimuth@gmail.com>
2022-04-24 22:35:41 -0300

    mount Arquivos at home folder

--- startup-script.sh
+++ startup-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo system76-power profile performance &
+sudo system76-power profile balanced &
 sudo brightnessctl -d "intel_backlight" set 30%
-sudo ntfsfix /dev/sda1 && sudo mount /dev/sda1 /mnt/Arquivos
+sudo ntfsfix /dev/sda1 && sudo mount /dev/sda1 /home/gustavolendimuth/Arquivos
